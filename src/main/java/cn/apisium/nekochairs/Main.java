11449a42988d588148883df354517158ef949258
Harukawa Sayaka <bbleae@outlook.com>
2022-03-22 22:53:07 +0800

    clean code

--- src/main/java/cn/apisium/nekochairs/Main.java
+++ src/main/java/cn/apisium/nekochairs/Main.java
@@ -1,5 +1,7 @@
 package cn.apisium.nekochairs;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
@@ -16,42 +18,41 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.annotation.permission.Permission;
-import org.bukkit.plugin.java.annotation.plugin.*;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
+import org.bukkit.plugin.java.annotation.plugin.Description;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.Website;
 import org.bukkit.plugin.java.annotation.plugin.author.Author;
 import org.spigotmc.event.entity.EntityDismountEvent;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 
 import static org.bukkit.event.block.Action.RIGHT_CLICK_BLOCK;
 
-@SuppressWarnings({ "deprecation", "unused" })
 @Plugin(name = "NekoChairs", version = "1.0")
 @Description("Make some chairs in Minecraft!")
 @Author("Shirasawa")
 @Website("https://apisium.cn")
 @Permission(name = "nekochairs.use", defaultValue = PermissionDefault.TRUE)
-@ApiVersion(ApiVersion.Target.v1_13)
+@ApiVersion(ApiVersion.Target.v1_18)
 public class Main extends JavaPlugin implements Listener {
     private final String NAME = "$$Chairs$$";
-    private final HashSet<ArmorStand> list = new HashSet<>();
+    private final HashSet<Entity> armorStandList = new HashSet<>();
 
     @Override
     public void onEnable() {
-        getServer().getScheduler().runTaskTimer(this, () ->
-            getServer().getWorlds().forEach(w -> w.getEntitiesByClasses(ArmorStand.class).forEach(this::check)),
-            100, 100);
+        getServer().getScheduler().runTaskTimer(this, () -> getServer().getWorlds().forEach(w -> w.getEntitiesByClasses(ArmorStand.class).forEach(this::check)), 100, 100);
         getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-        list.forEach(it -> {
+        armorStandList.forEach(it -> {
             it.getPassengers().forEach(Entity::leaveVehicle);
             it.remove();
         });
-        list.clear();
+        armorStandList.clear();
     }
 
     @EventHandler
@@ -67,9 +68,8 @@ public class Main extends JavaPlugin implements Listener {
     void onPlayerInteract(final PlayerInteractEvent e) {
         final Block b = e.getClickedBlock();
         final Player p = e.getPlayer();
-        if (p.getGameMode() == GameMode.SPECTATOR || e.getItem() != null || e.getAction() != RIGHT_CLICK_BLOCK ||
-            b == null || !p.hasPermission("nekochairs.use") ||
-            b.getType().data != Stairs.class) return;
+        if (p.getGameMode() == GameMode.SPECTATOR || e.getItem() != null || e.getAction() != RIGHT_CLICK_BLOCK || b == null || !p.hasPermission("nekochairs.use") || b.getType().data != Stairs.class)
+            return;
         final Stairs data = (Stairs) b.getBlockData();
         if (data.getHalf() == Bisected.Half.TOP) return;
         final Location l = b.getLocation().clone().add(0.5, -1.18, 0.5);
@@ -88,51 +88,42 @@ public class Main extends JavaPlugin implements Listener {
 
         final ArmorStand a = (ArmorStand) b.getWorld().spawnEntity(l, EntityType.ARMOR_STAND);
         a.setAI(false);
-        a.setCustomName(NAME);
+        a.customName(Component.text(NAME));
         a.setCanMove(false);
         a.setBasePlate(false);
         a.setCanTick(false);
         a.setVisible(false);
         a.setCanPickupItems(false);
-        a.setPassenger(p);
-        list.add(a);
+        a.addPassenger(p);
+        armorStandList.add(a);
     }
 
     @EventHandler
     void onEntityDismount(final EntityDismountEvent e) {
         final Entity l = e.getDismounted();
-        final String name = l.getCustomName();
-        if (l instanceof ArmorStand && name != null && name.equals(NAME)) leaveChair(l, e.getEntity());
+        final TextComponent name = (TextComponent) l.customName();
+        if (l instanceof ArmorStand && name != null && name.content().equals(NAME)) leaveChair(l, e.getEntity());
     }
 
-    @SuppressWarnings("SuspiciousMethodCalls")
     private void leaveChair(final Entity l, final Entity p) {
-        list.remove(l);
+        armorStandList.remove(l);
         l.remove();
         getServer().getScheduler().runTaskLater(this, () -> {
-            final Entity p2 = p == null ? l.getPassenger() : p;
+            final Entity p2 = p == null ? l.getPassengers().get(0) : p;
             if (p2 == null) return;
             p2.teleport(p2.getLocation().add(0.0, 0.5, 0.0));
         }, 1);
     }
 
-    @SuppressWarnings("SuspiciousMethodCalls")
     private boolean check(final Entity it) {
-        final String name = it.getCustomName();
-        final Entity p = it.getPassenger();
-        if (name != null && name.equals(NAME)) {
-            if (p != null && p.getVehicle() == it && it.getLocation().clone().add(-0.5, 1.18, -0.5)
-                .getBlock().getType().data == Stairs.class) return true;
+        final TextComponent name = (TextComponent) it.customName();
+        final Entity p = it.getPassengers().get(0);
+        if (name != null && name.content().equals(NAME)) {
+            if (p != null && p.getVehicle() == it && it.getLocation().clone().add(-0.5, 1.18, -0.5).getBlock().getType().data == Stairs.class)
+                return true;
             it.remove();
-            list.remove(it);
+            armorStandList.remove(it);
         }
         return false;
-    }
-
-    private ArrayList<ArmorStand> getChairsNearBy(final Location l) {
-        final Collection<ArmorStand> entities = l.getNearbyEntitiesByType(ArmorStand.class, 0.5, 0.5, 0.5);
-        final ArrayList<ArmorStand> list = new ArrayList<>();
-        for (ArmorStand it : entities) if (check(it)) list.add(it);
-        return list;
     }
 }
